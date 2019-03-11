39b931cd6108fa282024472f77294729eb9ad5af
EfrenSango <efren.sanchezgon@gmail.com>
2019-03-11 22:18:05 +0100

    Updates

---
+++ precache-manifest.77b9c59d6c85b20bff44729f9836468b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b954becc4f19043e5648",
+    "url": "/hackernews-client/static/css/main.36e8b4e8.chunk.css"
+  },
+  {
+    "revision": "b954becc4f19043e5648",
+    "url": "/hackernews-client/static/js/main.7d54f0c9.chunk.js"
+  },
+  {
+    "revision": "a2a141c300e4b66981e3",
+    "url": "/hackernews-client/static/js/runtime~main.4645b201.js"
+  },
+  {
+    "revision": "74f5631bc6688db5b3e2",
+    "url": "/hackernews-client/static/js/2.8419fb5c.chunk.js"
+  },
+  {
+    "revision": "cf518b7fea2cda78fa8d3a773d17459c",
+    "url": "/hackernews-client/index.html"
+  }
+];
